f6d12411cc57b26096e334587e4de88b21fdfa08
Roaan Vos <rvos@atlassian.com>
2020-04-04 14:03:52 +1100

    Setup amplify

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-api-dev-attachmentsbucket-1ss1mj4e4d6ro"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://xoh8ekrogl.execute-api.ap-southeast-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_UBcc8kWWR",
+    APP_CLIENT_ID: "651alt2e4hi74f0qpbfeks4895",
+    IDENTITY_POOL_ID: "ap-southeast-2:9cb67888-a436-4494-8d8a-ae2949f8c351"
+  }
+};
